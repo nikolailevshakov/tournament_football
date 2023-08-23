7aa3ecf4432b6adf0fa41010c8d74192807c1693
Levshakov Nikolai <levshakov_nikk@bk.ru>
2023-08-23 10:12:44 +0200

    added env.py module

---
+++ bot/envs.py
@@ -0,0 +1,8 @@
+import os
+
+POSTGRES_USER = os.environ["POSTGRES_USER"]
+POSTGRES_PASSWORD = os.environ["POSTGRES_PASSWORD"]
+DATABASE_HOST = os.environ["DATABASE_HOST"]
+DATABASE_PORT = os.environ["DATABASE_PORT"]
+DATABASE = os.environ["DATABASE"]
+TOKEN = os.environ["TOKEN"]
--- infra/variables.tf
+++ infra/variables.tf
@@ -20,7 +20,7 @@ variable "subnet_cidr_block" {
 variable "my_ip" {
   description = "My ip address"
   type = string
-  default = "87.116.162.65"
+  default = "87.116.165.177"
 }
 
 variable "ami" {
