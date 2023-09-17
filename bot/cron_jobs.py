06b24b52b75d076636d833969173b0965b6a3437
Levshakov Nikolai <levshakov_nikk@bk.ru>
2023-09-17 14:56:27 +0200

    updated cron jobs

--- bot/cron_jobs.py
+++ bot/cron_jobs.py
@@ -1,14 +1,13 @@
 import channel_posts
-import schedule
 import jobs
 import utils
-import time
+import sys
 
-schedule.every().monday.at("20:00").do(channel_posts.post_results())
-schedule.every().tuesday.at("20:00").do(jobs.clear_current_week())
-schedule.every().wednesday.at("20:00").do(channel_posts.post_with_header("Игры этой недели!", utils.read_games()))
-schedule.every().friday.at("20:00").do(channel_posts.check_if_prediction_exists())
-
-while True:
-    schedule.run_pending()
-    time.sleep(1)
+if sys.argv[1] == "monday":
+    channel_posts.post_results()
+elif sys.argv[1] == "tuesday":
+    jobs.clear_current_week()
+elif sys.argv[1] == "wednesday":
+    channel_posts.post_with_header("Игры этой недели!", utils.read_games())
+elif sys.argv[1] == "friday":
+    channel_posts.check_if_prediction_exists()
