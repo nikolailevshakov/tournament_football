92552259a4bbdacdc49129d0cb80b8ba40bebb0e
Levshakov Nikolai <levshakov_nikk@bk.ru>
2023-10-17 23:13:46 +0200

    Fixed pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -53,7 +53,7 @@ resource "aws_security_group" "instance" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["87.116.0.0/16"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   ingress {
     from_port   = 80
